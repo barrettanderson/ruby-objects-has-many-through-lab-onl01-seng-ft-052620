28cdcbd247fbe103c510178b98eb5793f52d308b
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-12 05:02:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,10 +21,10 @@ class Artist
     end
   end
 
-  # def genres
-  #   songs.map do |song|
-  #     song.genre
-  #   end
-  # end
+  def genres
+    songs.map do |song|
+      song.genre
+    end
+  end
 
 end
