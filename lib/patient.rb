63fd8e639a09ea28d428873eed0c605695924da8
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-12 05:27:54 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,4 +15,10 @@ class Patient
     Appointment.new(date, self, doctor)
   end
 
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
 end
